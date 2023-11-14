833fb56e4d5dcf078b92b99de26a1c4f21475ecf
Carlosviin <carlosvin26@gmail.com>
2023-11-14 16:35:22 -0300

    Codigo melhorado

--- Aula2/script.js
+++ Aula2/script.js
@@ -1,21 +1,23 @@
 var numeroSecreto = parseInt((Math.random () * 1000)+1); 
-var numeroDeTentativas = 0
+var numeroDeTentativas = 0;
+var tentativasRestantes = 10;
 
 while(chute != numeroSecreto){
     numeroDeTentativas = numeroDeTentativas + 1;
-    var chute = prompt('Digite um numero entre 1 e 1000');
+    var chute = prompt('Digite um numero entre 1 e 1000. Você tem ' + tentativasRestantes + ' tentativas restantes');
+    tentativasRestantes = tentativasRestantes - 1;
+
     if (chute == numeroSecreto) {
-        alert('acertou!');
+        alert(':D acertou! Você usou ' + numeroDeTentativas + ' tentativas');
 
     }else if (chute > numeroSecreto){  
-        alert ('Errou :( O numero secreto é menor');
+        alert (':( Errou 1O numero secreto é menor');
 
     } else if (chute < numeroSecreto){
-        alert ('Errou :( O numero secreto é maior')
+        alert (':( Errou O numero secreto é maior')
     }
     if(numeroDeTentativas == 10){
-        alert('Suas tentativas acabaram');
+        alert(':( Você perdeu... suas tentativas acabaram');
         break;        
     }
 }
-alert ('Você tentou ' + numeroDeTentativas + ' tentativas')
